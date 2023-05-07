8f46b7710c9f2eb4e7a11f497f112faa89eccb5d
przemoai <again364@gmail.com>
2023-05-07 21:35:52 +0200

    [7] Change product dtos to be records

--- product-service/src/main/java/mroczek/product/product/dto/ProductRequestDto.java
+++ product-service/src/main/java/mroczek/product/product/dto/ProductRequestDto.java
@@ -1,15 +1,11 @@
 package mroczek.product.product.dto;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.math.BigDecimal;
 
-@Builder
-@Value
-public class ProductRequestDto {
-    String id;
-    String name;
-    String description;
-    BigDecimal price;
+public record ProductRequestDto(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
 }
--- product-service/src/main/java/mroczek/product/product/dto/ProductResponseDto.java
+++ product-service/src/main/java/mroczek/product/product/dto/ProductResponseDto.java
@@ -1,15 +1,11 @@
 package mroczek.product.product.dto;
 
-import lombok.Builder;
-import lombok.Value;
-
 import java.math.BigDecimal;
 
-@Builder
-@Value
-public class ProductResponseDto {
-    String id;
-    String name;
-    String description;
-    BigDecimal price;
+public record ProductResponseDto(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
 }
