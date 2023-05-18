eadf527d0ccbaee9fe18c64028cd1b852eef7bfc
przemoai <again364@gmail.com>
2023-05-18 17:05:42 +0200

    [25] Add zipkin tracer to trace add order process

--- order-service/src/main/java/mroczek/order/order/domain/OrderFacade.java
+++ order-service/src/main/java/mroczek/order/order/domain/OrderFacade.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mroczek.order.order.dto.InventoryResponseDto;
 import mroczek.order.order.dto.OrderDto;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,6 +23,7 @@ public class OrderFacade {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     @Transactional
     public String placeOrder(OrderDto orderDto) {
@@ -39,23 +42,31 @@ public class OrderFacade {
                 .toList();
 
 
-        InventoryResponseDto[] inventoryResponse = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/v1/inventory",
-                        uriBuilder -> uriBuilder.queryParam("sku-code", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponseDto[].class)
-                .block();
+        Span inventoryFacadeLookup = tracer.nextSpan().name("InventoryFacadeLookup ");
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryFacadeLookup.start())) {
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponse)
-                .allMatch(InventoryResponseDto::isInStock);
+            InventoryResponseDto[] inventoryResponse = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/v1/inventory",
+                            uriBuilder -> uriBuilder.queryParam("sku-code", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponseDto[].class)
+                    .block();
 
-        if (allProductsInStock) {
-            Order save = orderRepository.save(order);
-            log.info("Save order {}", save.getId());
-            return "Order placed successfully!";
-        } else {
-            return "Product out of stock";
+            boolean allProductsInStock = Arrays.stream(inventoryResponse)
+                    .allMatch(InventoryResponseDto::isInStock);
+
+            if (allProductsInStock) {
+                Order save = orderRepository.save(order);
+                log.info("Save order {}", save.getId());
+                return "Order placed successfully!";
+            } else {
+                return "Product out of stock";
+            }
+
+        } finally {
+            inventoryFacadeLookup.end();
         }
+
 
     }
 
