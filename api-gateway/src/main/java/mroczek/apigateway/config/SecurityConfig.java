b3b10fbe5ca1faf1c19c5db3e56d335e78a5069d
przemoai <again364@gmail.com>
2023-05-18 11:21:33 +0200

    [23] Add spring security keycload in gateway

---
+++ api-gateway/src/main/java/mroczek/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package mroczek.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
+                .csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .build();
+    }
+}
