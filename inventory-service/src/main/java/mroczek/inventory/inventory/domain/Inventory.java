d1e867706c9f4246fe6e63bf0f8b5a85312c61b6
przemoai <again364@gmail.com>
2023-05-18 16:45:13 +0200

    Downgrade springboot to 2.7.11 to be enable to use Spring Sleuth

--- inventory-service/src/main/java/mroczek/inventory/inventory/domain/Inventory.java
+++ inventory-service/src/main/java/mroczek/inventory/inventory/domain/Inventory.java
@@ -1,15 +1,16 @@
 package mroczek.inventory.inventory.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "inventories")
--- order-service/src/main/java/mroczek/order/order/domain/OrderRepository.java
+++ order-service/src/main/java/mroczek/order/order/domain/OrderRepository.java
@@ -1,6 +1,8 @@
 package mroczek.order.order.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface OrderRepository extends JpaRepository<Order, Long> {
 }
